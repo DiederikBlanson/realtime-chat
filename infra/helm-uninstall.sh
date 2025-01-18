84aabe60e9abe0e7022620da2cdccd4ee573fd77
Diederik Blanson <diederikblanson@outlook.com>
2025-01-18 23:08:34 +0100

    rename services and add kubernetes configurations

---
+++ infra/helm-uninstall.sh
@@ -0,0 +1,8 @@
+#helm uninstall rabbitmq
+#helm uninstall redis-container
+#helm uninstall cassandra
+helm uninstall chat-messages
+helm uninstall chat-presence
+helm uninstall chat-sd
+helm uninstall chat-web
+helm uninstall chat-ws
